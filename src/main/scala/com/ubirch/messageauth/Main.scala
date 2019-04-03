10ad90f8397ed61b1dc99a4f8377bf8c06b6c820
mrobakowski <mikolaj.robakowski@ubirch.com>
2019-04-03 17:40:41 +0200

    Actually shut JVM down after a critical error to let kubernetes restart it

--- src/main/scala/com/ubirch/messageauth/Main.scala
+++ src/main/scala/com/ubirch/messageauth/Main.scala
@@ -2,6 +2,6 @@ package com.ubirch.messageauth
 
 object Main {
   def main(args: Array[String]): Unit = {
-    new MessageAuthMicroservice(new AuthCheckers(_).getDefault).runUntilDone
+    new MessageAuthMicroservice(new AuthCheckers(_).getDefault).runUntilDoneAndShutdownProcess
   }
 }
