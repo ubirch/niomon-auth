a7e820136e6e0430f97b24c1afd1c2a0c359cf68
Carlos Sanchez <caaugustoss@gmail.com>
2020-07-17 16:27:12 +0200

    UP-2007 Unifying names for record

--- src/main/scala/com/ubirch/messageauth/MessageAuthMicroservice.scala
+++ src/main/scala/com/ubirch/messageauth/MessageAuthMicroservice.scala
@@ -13,20 +13,20 @@ class MessageAuthMicroservice(authCheckerFactory: NioMicroservice.Context => Aut
   val authorizedTopic: String = outputTopics("authorized")
   val unauthorizedTopic: String = outputTopics("unauthorized")
 
-  def processRecord(input: ConsumerRecord[String, Array[Byte]]): ProducerRecord[String, Array[Byte]] = {
+  def processRecord(record: ConsumerRecord[String, Array[Byte]]): ProducerRecord[String, Array[Byte]] = {
 
-    val requestId = input.requestIdHeader().orNull
+    val requestId = record.requestIdHeader().orNull
 
-    val CheckResult(rejectionReason, headersToAdd) = checkAuth(input.headersScala)
+    val CheckResult(rejectionReason, headersToAdd) = checkAuth(record.headersScala)
 
     rejectionReason match {
       case None =>
         logger.info(s"request [{}] is authorized", v("requestId", requestId))
-        input.toProducerRecord(authorizedTopic).withExtraHeaders(headersToAdd.toSeq: _*)
+        record.toProducerRecord(authorizedTopic).withExtraHeaders(headersToAdd.toSeq: _*)
       case Some(reason) =>
         logger.info(s"request [{}] is NOT authorized; reason: {}", v("requestId", requestId),
           v("notAuthorizedReason", reason.getMessage))
-        input.toProducerRecord(unauthorizedTopic).withExtraHeaders("http-status-code" -> "401")
+        record.toProducerRecord(unauthorizedTopic).withExtraHeaders("http-status-code" -> "401")
     }
   }
 }
